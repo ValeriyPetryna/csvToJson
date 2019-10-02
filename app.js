78f67c0f59b86366f7c705c7daa1779dc5df9b3a
ValeriyPetryna <valeriypetrina@gmail.com>
2019-10-03 02:42:57 +0300

    unzip files and create output.json for mini test data

--- app.js
+++ app.js
@@ -1,70 +1,44 @@
 const fs = require('fs');
-const csvjson = require('csvjson');
-const path = require('path');
-const moment = require('moment');
+const csvjson = require('csvjson'); // read and parse csv files
+const moment = require('moment'); // format dates
+const JSZip = require('jszip'); // read zip archive
 
-let parsedUsers = [];
-const data = fs.readFileSync(path.join(__dirname, 'users1.csv'), { encoding: 'utf8' });
-
-const options = {
-  delimiter: '||', // optional
-  quote: '""', // optional
-};
-parsedUsers = csvjson.toObject(data, options);
-
-// console.log(parsedUsers)
-
-const outputData = parsedUsers.map(user => {
-  return {
-    name: user.name,
-    phone: user.phone.replace(/\D+/g, ''),
-    person: {
-      firstName: user.first_name,
-      lastName: user.last_name,
-    },
-    amount: +user.amount,
-    date: moment(user.date, 'DD/MM/YYYY').format('YYYY-MM-DD'), // new Date(user.date).toISOString().split('T')[0] not working correctly
-    datee: new Date(user.date).toISOString().split('T')[0],
-    costCenterNum: user.cc.replace(/\D+/g, ''),
-  };
-});
-// console.log(outputData);
-
-/*
-var d = new Date('2017-09-21T21:00:00.000Z');
-dateformat(d, 'DD-MM-YYYY')
-console.log(Date.parse(str))
-const JSZip = require('jszip');
-const readFile = require('fs').readFile;
-read a zip files
-fs.readFile("testData.zip", function(err, data) {
-    if (err) throw err;
-    JSZip.loadAsync(data).then(function (zip) {
-      files = Object.keys(zip.files);
-      files.forEach(element => {
-        zip.file(element).async("string").then(function (data) {
-            console.log(data);
-        });
+fs.readFile('testData.zip', function(err, data) {
+  if (!err) {
+    const outputData = []; // output array
+    var zip = new JSZip();
+    zip.loadAsync(data).then(function(contents) {
+      Object.keys(contents.files).forEach(function(filename) {
+        zip
+          .file(filename)
+          .async('nodebuffer')
+          .then(function(content) {
+            fs.writeFileSync(filename, content);
+            const data = fs.readFileSync(filename, 'utf8');
+            const options = {
+              delimiter: '||',
+              quote: '""',
+            };
+            parsedUsers = csvjson.toObject(data, options); // convert csv file to object
+            let output = parsedUsers.map(user => {
+              return {
+                name: user.name,
+                phone: user.phone.replace(/\D+/g, ''), // cut extra symbols
+                person: {
+                  firstName: user.first_name,
+                  lastName: user.last_name,
+                },
+                amount: +user.amount,
+                date: moment(user.date, 'DD/MM/YYYY').format('YYYY-MM-DD'), // also can use pure js methods, something like this : new Date(user.date).toISOString().split('T')[0]
+                costCenterNum: user.cc.slice(3), // slise prefix
+              };
+            });
+            outputData.push(...output); // push file elements (users) to output array
+            const file = JSON.stringify(outputData);
+            fs.writeFileSync('output.json', file); // write output array to json file
+          });
       });
-      console.log(files);
+      console.log('output.json was successfully created!');
     });
-});
-
-readFile('./users1.csv', 'utf-8', (err, fileContent) => {
-  if(err) {
-      console.log(err); // Do something to handle the error or just throw it
-      throw new Error(err);
   }
-  const jsonObj = csvjson.toObject(fileContent);
-  console.log(jsonObj);
 });
-const createReadStream = require('fs').createReadStream;
-const createWriteStream = require('fs').createWriteStream;
-const csvjson = require('csvjson');
-const toObject = csvjson.stream.toObject();
-const stringify = csvjson.stream.stringify();
-createReadStream('./users1.csv', 'utf-8')
-    .pipe(toObject)
-    .pipe(stringify)
-    .pipe(createWriteStream('./test-data-output-stream.json'));
-*/
